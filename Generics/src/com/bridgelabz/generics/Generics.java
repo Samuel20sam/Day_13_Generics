0d452cc6d5a41668dbe9e4aa0ce7d9144c044976
Samuel_Elijah <samuel20sam@gmail.com>
2022-06-17 18:28:00 +0530

    Refracting to Generics

--- Generics/src/com/bridgelabz/generics/Generics.java
+++ Generics/src/com/bridgelabz/generics/Generics.java
@@ -3,33 +3,24 @@ package com.bridgelabz.generics;
 import java.util.Scanner;
 
 public class Generics {
-    private void checkMaxInt(Integer x1, Integer x2, Integer x3) {
-        System.out.println("Max of " +x1 +", " +x2 +" & " +x3 +" is " +Math.max(x3,(Math.max(x1,x2))));
-    }
-    private void checkMaxFloat(Float f1, Float f2, Float f3) {
-        System.out.println("Max of " +f1 +", " +f2 +" & " +f3 +" is " +Math.max(f3,(Math.max(f1,f2))));
-    }
-    private void checkMaxString(String s1, String s2, String s3) {
-        String max = s1;
-        if (s2.compareTo(max) > 0)
-            max = s2;
-        if (s3.compareTo(max) > 0)
-            max = s3;
-        System.out.println("Max of strings " +s1 + ", " +s2 + " & " +s3 +" is " +max);
-        System.out.println("Max of " + s1 +", " +s2 +" & " +s3 +" is "
-                +Math.max(s3.length(),(Math.max(s1.length(),s2.length()))));
+        public static <T extends Comparable<T>> void checkMax(T x,T y,T z){
+        T max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+        if (z.compareTo(max) > 0)
+            max = z;
+        System.out.println("Max of " +x +", " +y +" & " +z +" is " +max);
     }
     public static void main(String[] args) {
-        Generics obj = new Generics();
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter three Integers");
-        Integer x1 = input.nextInt(), x2 = input.nextInt(), x3 = input.nextInt();
-        obj.checkMaxInt(x1, x2, x3);
-        System.out.println("Enter three Floats");
-        Float f1 = input.nextFloat(), f2 = input.nextFloat(), f3 =input.nextFloat();
-        obj.checkMaxFloat(f1, f2, f3);
-        System.out.println("Enter three Strings");
+        System.out.println("Enter three integer");
+        Integer i1 = input.nextInt(), i2 = input.nextInt(), i3 = input.nextInt();
+        System.out.println("Enter three float");
+        Float f1 = input.nextFloat(), f2 = input.nextFloat(), f3 = input.nextFloat();
+        System.out.println("Enter three float");
         String s1 = input.next(), s2 = input.next(), s3 = input.next();
-        obj.checkMaxString(s1, s2, s3);
+        checkMax(i1, i2, i3);
+        checkMax(f1, f2, f3);
+        checkMax(s1, s2, s3);
     }
 }
