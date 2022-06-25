67ad8108dc0b8e018b2e4bfcf0fadf21bfabb30c
Samuel_Elijah <samuel20sam@gmail.com>
2022-06-25 17:33:48 +0530

    Adding UC5

--- Generics/src/com/bridgelabz/generics/Generics.java
+++ Generics/src/com/bridgelabz/generics/Generics.java
@@ -3,19 +3,16 @@ package com.bridgelabz.generics;
 import java.util.Scanner;
 
 public class Generics <T extends Comparable <T>> {
+
     public T x, y, z;
 
-    public Generics(T x,T y,T z)
-    {
+    public Generics(T x, T y, T z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
-    public void testMaximum() {
-        Generics.checkMax(this.x,this.y,this.z);
-    }
 
-    public static <T extends Comparable<T>> void checkMax(T x,T y,T z){
+    public static <T extends Comparable<T>> void checkMax(T x, T y, T z){
         T max = x;
         if (y.compareTo(max) > 0)
             max = y;
@@ -34,8 +31,8 @@ public class Generics <T extends Comparable <T>> {
         Float f1 = input.nextFloat(), f2 = input.nextFloat(), f3 = input.nextFloat();
         System.out.println("Enter three strings");
         String s1 = input.next(), s2 = input.next(), s3 = input.next();
-        new Generics<Integer>(i1,i2,i3).testMaximum();
-        new Generics<Float>(f1,f2,f3).testMaximum();
-        new Generics<String>(s1,s2,s3).testMaximum();
+        checkMax(i1,i2,i3);
+        checkMax(f1,f2,f3);
+        checkMax(s1,s2,s3);
     }
 }
